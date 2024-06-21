9a97f8401248143d72370bb18227bac7933d5e0e
Niyas Ny <niyasny433@gmail.com>
2024-06-21 23:26:47 +0530

    started timezone page

--- lib/view/timezonescreen.dart
+++ lib/view/timezonescreen.dart
@@ -6,6 +6,8 @@ class TimeZoneScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(),
+    );
   }
 }
