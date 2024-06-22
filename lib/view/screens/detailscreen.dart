db4e2df23155058018369d3c0e4e01c0347bcb51
Niyas Ny <niyasny433@gmail.com>
2024-06-22 20:26:50 +0530

    updated details screen page

--- lib/view/screens/detailscreen.dart
+++ lib/view/screens/detailscreen.dart
@@ -9,7 +9,6 @@ class DetailsScreen extends StatelessWidget {
   const DetailsScreen({super.key, required this.movie});
 
   final Movie movie;
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
